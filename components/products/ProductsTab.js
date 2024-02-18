003f070f079bfb53b287065c015b320be47599f3
rezabox <reza.asareh81@gmail.com>
2024-02-18 08:32:15 +0330

    add react-tab filter

--- components/products/ProductsTab.js
+++ components/products/ProductsTab.js
@@ -1,9 +1,12 @@
+"use client"
 import Image from "next/image";
 import Image1 from '../../app/images/b1.jpg';
 import Image2 from '../../app/images/p4.jpg';
 import Image3 from '../../app/images/p1.jpg';
 import Product from "./Product";
 import Link from "next/link";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const ProductsTab = ({tabList,tabPanel}) => {
   return (
@@ -13,24 +16,28 @@ const ProductsTab = ({tabList,tabPanel}) => {
           <div className="heading_container heading_center">
             <h2>منو محصولات</h2>
           </div>
-
-          <ul className="filters_menu">
+          <Tabs>
+          <TabList>
+             <ul className="filters_menu">
             {tabList.map((item,index)=> (
-              <li key={index}>{item}</li>
+              <Tab key={index}>{item}</Tab>
             ))}
-          </ul>
-
+             </ul>
+          </TabList>
           <div className="filters-content">
            {tabPanel.map((item, index)=>(
-            <div key={index} className="row grid">
+            <TabPanel key={index}>
+            <div className="row grid">
               {item.map(product => (
                 <div key={product.id} className="col-sm-6 col-lg-4">
                   <Product product={product}/>
                 </div>
               ))}
             </div>
+            </TabPanel>
             ))}
           </div>
+          </Tabs>
           <div className="btn-box">
             <Link href='/menu'>مشاهده بیشتر</Link>
           </div>
