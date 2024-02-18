b6e6ace7ea01b3519b086dd6fe5369aadee72e89
rezabox <reza.asareh81@gmail.com>
2024-02-18 13:04:20 +0330

    bugfixed app/page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,17 @@
+import About from "@/components/About";
 import Features from "@/components/Features";
+import Contact from "@/components/contact/ContactSS";
 import ProductsTab from "@/components/products/ProductsTab";
 import { getFetch } from "@/utils/fetch";
-
 export default async function Home() {
   const productList = await getFetch('/products/products-tabs');
   return (
     <>
      <Features/>
      <ProductsTab tabList={productList.tabList} tabPanel={productList.tabPanel}/>
+     <About/>
+     <Contact/>
     </>
   );
-
 
 }
