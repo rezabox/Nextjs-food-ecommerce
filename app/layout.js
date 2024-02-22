fd79e8ebb967b0f012b6bca3d666a198617647b2
rezabox <reza.asareh81@gmail.com>
2024-02-22 18:56:15 +0330

    updated layout page

--- app/layout.js
+++ app/layout.js
@@ -3,15 +3,18 @@ import "./globals.css";
 import Header from "@/components/header/header";
 import Toastify from "@/components/libraries/Toastify"
 import Footer from "@/components/footer/Footer";
+import Loader from "./loader/Loader";
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body>
+        <Loader>
         <Header/>
         {children}
         <Footer/>
         <BootstrapClient/>
         <Toastify/>
+        </Loader>
       </body>
     </html>
   );
