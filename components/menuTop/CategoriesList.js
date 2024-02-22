62518f98adab43b6eb0be63952be59a7f9258108
rezabox <reza.asareh81@gmail.com>
2024-02-22 18:49:51 +0330

    add TopMenu

---
+++ components/menuTop/CategoriesList.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+function CategoriesList({categories}) {
+  return (
+    <div className="filter-list">
+    <div className="form-label">دسته بندی</div>
+    <ul>
+      {categories.map(category =>(
+        <li key={category.id} className="my-2 cursor-pointer">{category.name}</li>
+      ))}
+    </ul>
+  </div>
+  )
+}
+
+export default CategoriesList;
