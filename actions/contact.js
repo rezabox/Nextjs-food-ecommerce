f56904bfe135db1c5546fa6cbd17565ae6de7f32
rezabox <reza.asareh81@gmail.com>
2024-02-23 22:16:46 +0330

    create from action pattern

--- actions/contact.js
+++ actions/contact.js
@@ -28,5 +28,20 @@ async function create(state, formData) {
         }
     }
 }
-
-export { create };
+async function login(stateLogin, formData){
+  const phone = formData.get('cellphone')
+  if(phone === ''){
+    return {
+      status: "error",
+      message: "شماره موبایل الزامی است."
+    }
+  }
+  const pattern = /^(\+98|0)?9\d{9}$/;
+  if(!pattern.test(phone)){
+    return {
+      status: "error",
+      message: "فرمت شماره موبایل درست نیست."
+    }
+  }
+}
+export { create, login };
