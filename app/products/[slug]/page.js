7fe54a796ef630ab8bd69cb3d563c6f0b6867835
rezabox <reza.asareh81@gmail.com>
2024-02-23 12:12:03 +0330

    updated app/product

---
+++ app/products/[slug]/page.js
@@ -0,0 +1,156 @@
+import { getFetch } from "@/utils/fetch";
+import { numberFormat, salePercent } from "@/utils/help";
+import React from "react";
+
+async function ProductPage({params}) {
+  const product = await getFetch(`/products/${decodeURI(params.slug)}`);
+  console.log(product);
+  return (
+    <div>
+      <section className="single_page_section layout_padding">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-10 offset-md-1">
+              <div className="row gy-5">
+                <div className="col-sm-12 col-lg-6">
+                  <h3 className="fw-bold mb-4">{product.name}</h3>
+                  <h5 className="mb-3">
+                    {product.is_sale ? (
+                                <>
+                                  <span>{numberFormat(product.sale_price)}</span>
+                                  <del>{numberFormat(product.price)}</del>
+                                </>
+                            ) : (
+                                <>
+                                  <span>{numberFormat(product.price)}</span>
+                                </>
+                            )} 
+                        <span>تومان</span>
+                        {product.is_sale && 
+                        <div className="text-danger fs-6">{salePercent(product.price , product.sale_price)}% تخفیف</div>
+                        }
+                  </h5>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است.
+                  </p>
+
+                  <div className="mt-5 d-flex">
+                    <button className="btn-add">افزودن به سبد خرید</button>
+                    <div className="input-counter ms-4">
+                      <span className="plus-btn">+</span>
+                      <div className="input-number">1</div>
+                      <span className="minus-btn">-</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-12 col-lg-6">
+                  <div
+                    id="carouselExampleIndicators"
+                    className="carousel slide"
+                    data-bs-ride="carousel"
+                  >
+                    <div className="carousel-indicators">
+                      <button
+                        type="button"
+                        data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide-to="0"
+                        className="active"
+                      ></button>
+                      <button
+                        type="button"
+                        data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide-to="1"
+                      ></button>
+                      <button
+                        type="button"
+                        data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide-to="2"
+                      ></button>
+                    </div>
+                    <div className="carousel-inner">
+                      <div className="carousel-item active">
+                        <img
+                          src="./images/p1.jpg"
+                          className="d-block w-100"
+                          alt="..."
+                        />
+                      </div>
+                      <div className="carousel-item">
+                        <img
+                          src="./images/p2.jpg"
+                          className="d-block w-100"
+                          alt="..."
+                        />
+                      </div>
+                      <div className="carousel-item">
+                        <img
+                          src="./images/p3.jpg"
+                          className="d-block w-100"
+                          alt="..."
+                        />
+                      </div>
+                    </div>
+                    <button
+                      className="carousel-control-prev"
+                      type="button"
+                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide="prev"
+                    >
+                      <span className="carousel-control-prev-icon"></span>
+                      <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button
+                      className="carousel-control-next"
+                      type="button"
+                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide="next"
+                    >
+                      <span className="carousel-control-next-icon"></span>
+                      <span className="visually-hidden">Next</span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <hr />
+      <section className="food_section my-5">
+        <div className="container">
+          <div className="row gx-3">
+            <div className="col-sm-6 col-lg-3">
+              <div className="box">
+                <div>
+                  <div className="img-box">
+                    <img className="img-fluid" src="./images/b1.jpg" alt="" />
+                  </div>
+                  <div className="detail-box">
+                    <h5>لورم ایپسوم متن</h5>
+                    <p>
+                      لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+                      و با استفاده از طراحان گرافیک است.
+                    </p>
+                    <div className="options">
+                      <h6>
+                        <del>45,000</del>
+                        34,000
+                        <span>تومان</span>
+                      </h6>
+                      <a href="">
+                        <i className="bi bi-cart-fill text-white fs-5"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default ProductPage;
