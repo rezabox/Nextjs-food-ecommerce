a902e5fc4d7c6f894af39549716c8f20af068060
rezabox <reza.asareh81@gmail.com>
2024-02-27 11:12:54 +0330

    updated Login/page

--- app/auth/Login/page.js
+++ app/auth/Login/page.js
@@ -1,35 +1,20 @@
 'use client';
-import { login } from "@/actions/contact";
-import SubmitButton from "@/components/SubmitButton";
-import React, { useEffect } from "react";
 
-import { useFormState } from 'react-dom'
-import { toast } from 'react-toastify';
+import CheckOtpForm from "@/components/auth/CheckOtpForm";
+import LoginForm from "@/components/auth/LoginForm";
+import { useState } from "react";
 
 function Login() {
-  const [stateLogin, formActionLogin] = useFormState(login, {});
-  useEffect(()=> {
-    toast(stateLogin?.message, { type: `${stateLogin?.status}` })
-  },[stateLogin])
+  const [setp, setStep] = useState(1);
+
   return (
     <div>
       <section className="auth_section book_section">
         <div className="container">
           <div className="row mt-5">
             <div className="col-md-4 offset-md-4">
-              <div className="card">
-                <div className="card-body">
-                  <div className="form_container">
-                    <form action={formActionLogin}>
-                      <div className="mb-3">
-                        <label className="form-label">شماره موبایل</label>
-                        <input name='cellphone' type="text" className="form-control" />
-                      </div>
-                      <SubmitButton title="ورود" style="btn btn-primary btn-auth"/>
-                    </form>
-                  </div>
-                </div>
-              </div>
+               {setp == 1 && <LoginForm setStep={setStep}/>}    
+               {setp == 2 && <CheckOtpForm/>}
             </div>
           </div>
         </div>
