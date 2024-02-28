7c5a3f7af1260ae1de8f4270a0908e05468b6435
rezabox <reza.asareh81@gmail.com>
2024-02-28 13:06:11 +0330

    updated middelware

--- middleware.js
+++ middleware.js
@@ -1,11 +1,10 @@
 import { NextResponse } from 'next/server';
-import  { NextRequest } from 'next/server';
 
 export function middleware(req) {
    const tokenData =  req.cookies.get('token');
    
    if(!tokenData) {
-     return NextRequest.redirect(new URL('/auth/login', req.url))
+     return NextResponse.redirect(new URL('/auth/login', req.url))
    }
 }
 
