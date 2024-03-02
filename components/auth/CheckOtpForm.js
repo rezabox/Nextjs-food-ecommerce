1f7099dddfd5c2a8e6c8ac61105d1ebb01e69e83
rezabox <reza.asareh81@gmail.com>
2024-03-02 12:56:53 +0330

    updated CheckOtp

--- components/auth/CheckOtpForm.js
+++ components/auth/CheckOtpForm.js
@@ -1,36 +1,39 @@
-'use client'
+"use client";
 import React, { useContext, useEffect, useReducer } from "react";
-import { useFormState } from 'react-dom'
-import { toast } from 'react-toastify';
+import { useFormState } from "react-dom";
+import { toast } from "react-toastify";
 import SubmitButton from "../SubmitButton";
 import { checkOtp } from "@/actions/contact";
 import AuthContext from "@/context/AuthContext";
 import { usePathname } from "next/navigation";
+import ResendButton from "./ResendButton";
+import { useRouter } from "next/navigation";
 
 function CheckOtpForm() {
- 
   const [stateLoginOtp, formActionOtp] = useFormState(checkOtp, {});
-  const { loginContext } = useContext(AuthContext)
-  useEffect(()=> {
+  const { loginContext } = useContext(AuthContext);
+  const router = useRouter();
+  useEffect(() => {
     toast(stateLoginOtp?.message, { type: `${stateLoginOtp?.status}` });
-    if(stateLoginOtp?.status === 'success'){
-       loginContext(stateLoginOtp.user);
+    if (stateLoginOtp?.status === "success") {
+      loginContext(stateLoginOtp.user);
+      router.push('/');
     }
-  },[stateLoginOtp])
-  
+  }, [stateLoginOtp]);
   return (
     <div className="card">
-    <div className="card-body">
-      <div className="form_container">
-        <form action={formActionOtp}>
-          <div className="mb-3">
-            <label className="form-label">کد ورود</label>
-            <input name='otp' type="text" className="form-control" />
-          </div>
-          <SubmitButton title="ورود" style="btn btn-primary btn-auth"/>
-        </form>
+      <div className="card-body">
+        <div className="form_container">
+          <form action={formActionOtp}>
+            <div className="mb-3">
+              <label className="form-label">کد ورود</label>
+              <input name="otp" type="text" className="form-control" />
+            </div>
+            <SubmitButton title="ورود" style="btn btn-primary btn-auth" />
+          </form>
+            <ResendButton />
+        </div>
       </div>
-    </div>
     </div>
   );
 }
