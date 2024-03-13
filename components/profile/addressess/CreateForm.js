a3ae98403a54e26d3710fe21d15cbaf53e8d2197
rezabox <reza.asareh81@gmail.com>
2024-03-13 11:48:41 +0330

    bugfixed Profile/addressess

--- components/profile/addressess/CreateForm.js
+++ components/profile/addressess/CreateForm.js
@@ -1,7 +1,17 @@
 'use client'
-import React, { useState } from 'react'
+import { useFormState } from "react-dom";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import React from "react";
+import SubmitButton from "@/components/SubmitButton";
+import { AddressCreate } from "@/actions/contact";
 
 function CreateForm({addresses, provinces, cities}) {
+  const [stateCreate, formActionCreate] = useFormState(AddressCreate, {});
+  useEffect(() => {
+    toast(stateCreate?.message, { type: `${stateCreate?.status}` });
+  }, [stateCreate]);
+
   const [citiesFilter, setCitiesFilter] = useState(cities);
   function changeProvince(e) {
     setCitiesFilter((cities.filter(city => city.province_id == e.target.value)));
@@ -17,24 +27,24 @@ function CreateForm({addresses, provinces, cities}) {
       >
         ایجاد آدرس جدید
       </button>
-      <div className="collapse mt-3" id="collapseExample">
+      <form action={formActionCreate} className="collapse mt-3" id="collapseExample">
         <div className="card card-body">
           <div className="row g-4">
             <div className="col col-md-6">
               <label className="form-label">عنوان</label>
-              <input type="text" className="form-control" />
+              <input name='title' type="text" className="form-control" />
             </div>
             <div className="col col-md-6">
               <label className="form-label">شماره تماس</label>
-              <input type="text" className="form-control" />
+              <input name='cellphone' type="text" className="form-control" />
             </div>
             <div className="col col-md-6">
               <label className="form-label">کد پستی</label>
-              <input type="text" className="form-control" />
+              <input name='postal_code' type="text" className="form-control" />
             </div>
             <div className="col col-md-6">
               <label className="form-label">استان</label>
-              <select className="form-select" onChange={changeProvince}>
+              <select name="province_id" className="form-select" onChange={changeProvince}>
                 {provinces.map(prow => (
                   <option key={prow.id} value={prow.id}>{prow.name}</option>
                 ))}
@@ -42,7 +52,7 @@ function CreateForm({addresses, provinces, cities}) {
             </div>
             <div className="col col-md-6">
               <label className="form-label">شهر</label>
-              <select className="form-select">
+              <select name="city_id" className="form-select">
                 {citiesFilter.map(city => (
                   <option key={city.id} value={city.id}>{city.name}</option> 
                 ))}
@@ -50,14 +60,14 @@ function CreateForm({addresses, provinces, cities}) {
             </div>
             <div className="col col-md-12">
               <label className="form-label">آدرس</label>
-              <textarea type="text" rows="5" className="form-control"></textarea>
+              <textarea name="address" type="text" rows="5" className="form-control"></textarea>
             </div>
           </div>
           <div>
-            <button className="btn btn-primary mt-4">ایجاد</button>
+            <SubmitButton title="ایجاد" style="btn btn-primary mt-4 btn-auth" />
           </div>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
