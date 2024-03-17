f1747e8f76053dccff3a4e8cc4c5e99049849f21
rezabox <reza.asareh81@gmail.com>
2024-03-17 08:41:50 +0330

    add delete button to form

---
+++ components/profile/addressess/DeleteForm.js
@@ -0,0 +1,27 @@
+"use client";
+
+import { deletedForm } from "@/actions/contact";
+import SubmitButton from "@/components/SubmitButton";
+import React, { useEffect, useState } from "react";
+import { useFormState } from "react-dom";
+import { toast } from "react-toastify";
+
+function DeleteForm({address}) {
+  const [stateDelete, formActionDelete] = useFormState(deletedForm, {});
+  useEffect(() => {
+    toast(stateDelete?.message, { type: `${stateDelete?.status}` });
+  }, [stateDelete]);
+
+  return (
+    <>
+      <div className="form-delete-address">
+        <form action={formActionDelete}>
+          <input type="hidden" value={address} name="address_id" />
+          <SubmitButton title="حذف" style="btn btn-dark" />
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default DeleteForm;
