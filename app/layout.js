b40c37ac40d781fd2e908c724eb3defc17308373
rezabox <reza.asareh81@gmail.com>
2024-03-30 09:11:28 +0330

    add Provider @redux to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,26 @@
 import BootstrapClient from "@/components/libraries/Bootstrap";
 import "./globals.css";
 import Header from "@/components/header/header";
-import Toastify from "@/components/libraries/Toastify"
+import Toastify from "@/components/libraries/Toastify";
 import Footer from "@/components/footer/Footer";
 import Loader from "./loader/Loader";
 import { AuthProvider } from "@/context/AuthContext";
+import ProviderRedux from "@/redux/ProviderRedux";
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body>
-       <AuthProvider>
-        <Loader>
-        <Header/>
-        {children}
-        <Footer/>
-        <BootstrapClient/>
-        <Toastify/>
-        </Loader>
-       </AuthProvider>
+        <AuthProvider>
+          <ProviderRedux>
+            <Loader>
+              <Header />
+              {children}
+              <Footer />
+              <BootstrapClient />
+              <Toastify />
+            </Loader>
+          </ProviderRedux>
+        </AuthProvider>
       </body>
     </html>
   );
