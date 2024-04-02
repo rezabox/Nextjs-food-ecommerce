8b7209f2ca51d5b58657141ff02bd452a3a1d6f7
rezabox <reza.asareh81@gmail.com>
2024-04-02 09:05:48 +0330

    add middleware to redux/store.js

--- redux/store.js
+++ redux/store.js
@@ -2,12 +2,25 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import { cartReducer } from "./slices/cardSlice";
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist'
 
+const persistConfig = {
+    key: 'root',
+    storage  
+}
+
+const persistedReducer = persistReducer(persistConfig, cartReducer)
 
 const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: false,
+  }),
   reducer: {
-     shoppingCard: cartReducer
+     shoppingCard: persistedReducer
   },
 });
-
 export default store;
+
+export const persistor = persistStore(store)
