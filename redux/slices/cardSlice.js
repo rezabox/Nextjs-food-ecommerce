3bb36427eb8dcf0a23537c0407a3ba443cbc2c73
rezabox <reza.asareh81@gmail.com>
2024-04-02 13:13:25 +0330

    updated slice cart

--- redux/slices/cardSlice.js
+++ redux/slices/cardSlice.js
@@ -1,6 +1,7 @@
 'use client'
 
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const initialState = {
     cart: []
@@ -17,9 +18,19 @@ export const cartSlice = createSlice({
       },
       removeFromCart: (state, action) => {
         state.cart = state.cart.filter(p => p.id !== action.payload)
+      },
+      increment: (state, action) => {
+        state.cart = state.cart.map(p => p.id === action.payload ? { ...p, qty: p.qty + 1 } : p)
+      },
+      decrement: (state, action) => {
+        state.cart = state.cart.map(p => p.id === action.payload ? { ...p, qty: p.qty - 1  } : p)
+      },
+      clear_item: (state, action) => {
+        state.cart = [];
+        toast.success('محصولات با موفقیت حذف شدند.')
       }
    }
 })
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, increment, decrement, deletedProduct, clear_item } = cartSlice.actions;
 export const  cartReducer  = cartSlice.reducer;
