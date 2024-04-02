3b4d006bc91b09fd10aae3c4b26aedf53d8c1a22
rezabox <reza.asareh81@gmail.com>
2024-04-02 09:05:11 +0330

    add ProviderRedux and redux-persist

--- redux/ProviderRedux.js
+++ redux/ProviderRedux.js
@@ -1,13 +1,15 @@
 'use client'
-
 import React from 'react'
 import { Provider } from 'react-redux';
-import store from './store';
+import store, { persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function ProviderRedux({children}) {
   return (
      <Provider store={store}>
-       {children}
+       <PersistGate loading={null} persistor={persistor}>
+          {children}
+       </PersistGate>  
      </Provider>
   )
 }
