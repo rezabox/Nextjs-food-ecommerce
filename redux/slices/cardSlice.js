38c98cb46ecf486ffb9e87fe3432646de9d07355
rezabox <reza.asareh81@gmail.com>
2024-04-06 11:54:56 +0330

    redux.js file sliceStore updated

--- redux/slices/cardSlice.js
+++ redux/slices/cardSlice.js
@@ -1,36 +1,58 @@
-'use client'
-
 import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 
 const initialState = {
-    cart: []
-}
+  cart: [],
+};
 
 export const cartSlice = createSlice({
-   name: 'cart',
-   initialState,
-   reducers: {
-      addToCart: (state, action) => {
-         const { product, qty } = action.payload;
-         state.cart = [...state.cart , { ...product, qty:qty }]
-         console.log(state.cart);
-      },
-      removeFromCart: (state, action) => {
-        state.cart = state.cart.filter(p => p.id !== action.payload)
-      },
-      increment: (state, action) => {
-        state.cart = state.cart.map(p => p.id === action.payload ? { ...p, qty: p.qty + 1 } : p)
-      },
-      decrement: (state, action) => {
-        state.cart = state.cart.map(p => p.id === action.payload ? { ...p, qty: p.qty - 1  } : p)
-      },
-      clear_item: (state, action) => {
-        state.cart = [];
-        toast.success('محصولات با موفقیت حذف شدند.')
-      }
-   }
-})
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const { product, qty } = action.payload;
+      state.cart = [...state.cart, { ...product, qty }];
+    },
+    removeFromCart: (state, action) => {
+      state.cart = state.cart.filter((p) => p.id !== action.payload);
+    },
+    removeFromCart2: (state, action) => {
+      state.cart = state.cart.filter((p) => p.id !== action.payload);
+      toast.success("محصول با موفقیت حذف گردید.");
+    },
+    increment: (state, action) => {
+      state.cart = state.cart.map((p) =>
+        p.id === action.payload ? { ...p, qty: p.qty + 1 } : p
+      );
+      toast.info("یک واحد به محصول اضافه شد.");
+      // Save cart to localStorage
+    },
+    decrement: (state, action) => {
+       state.cart = state.cart.map((p) =>
+        p.id === action.payload ? { ...p, qty: p.qty - 1 } : p
+      );
+      localStorage.setItem("cartItems", JSON.stringify(state.cart));
+      toast.info("یک واحد از محصول کم شد.");
+    },
+    clear_item: (state, action) => {
+      state.cart = [];
+      localStorage.clear(); // Clear cart from localStorage
+      toast.success("محصولات با موفقیت حذف شدند.");
+    },
+  },
+});
 
-export const { addToCart, removeFromCart, increment, decrement, deletedProduct, clear_item } = cartSlice.actions;
-export const  cartReducer  = cartSlice.reducer;
+
+
+export const {
+  addToCart,
+  removeFromCart,
+  increment,
+  decrement,
+  clear_item,
+  removeFromCart2,
+} = cartSlice.actions;
+export const cartReducer = cartSlice.reducer;
+
+// Load cart from localStorage when Redux store initializes
+
