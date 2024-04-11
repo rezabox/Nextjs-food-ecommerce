9578112a2d291d58dfdde88e756f5b15ce5227df
rezabox <reza.asareh81@gmail.com>
2024-04-11 14:41:51 +0330

    updated middleware

--- middleware.js
+++ middleware.js
@@ -4,10 +4,10 @@ export function middleware(req) {
    const tokenData =  req.cookies.get('token');
    
    if(!tokenData) {
-     return NextResponse.redirect(new URL('/auth/login', req.url))
+     return NextResponse.redirect(new URL('/auth/Login', req.url))
    }
 }
 
 export const config = {
-  matcher: ['/profile/:path*'],
+  matcher: ['/profile/:path*','/cart'],
 }
