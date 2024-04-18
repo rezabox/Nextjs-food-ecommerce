fc6459828657017eba25ff1ccf0bb74e2fc27bf3
rezabox <reza.asareh81@gmail.com>
2024-04-18 18:44:21 +0330

    app/profile

--- app/profile/page.js
+++ app/profile/page.js
@@ -5,6 +5,7 @@ import Profile from "./Profile";
 
 async function ProfilePage() {
   const token = cookies().get("token");
+  console.log(token);
   const user = await getFetch(
     "/profile/info",
     { Authorization: `Bearer ${token.value}` }
